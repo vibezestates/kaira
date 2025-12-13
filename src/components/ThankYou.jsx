e8c3e796f547a11a40a89d0dc8aaec48a3ab777e
ravisdofficial <ravisdofficial@gmail.com>
2025-12-13 15:18:27 +0530

    latest thank you

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -19,13 +19,13 @@ export default function Welcome() {
     >
       <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-10 px-6 sm:px-10 md:px-16">
         {/* Left Content */}
-        <div className="w-full md:w-1/2 text-center md:text-left">
+        <div className="w-full text-center md:text-left">
           <h2 className="text-4xl font-secondary sm:text-6xl md:text-5xl font-bold tracking-tighter pb-6 text-brown">
             Thank you !
           </h2>
 
           <p className="text-base sm:text-lg md:text-xl text-brown leading-relaxed mb-2 md:mb-6">
-           out team will contact you soon.
+          Our team will contact you soon.
           </p>
 
         </div>
